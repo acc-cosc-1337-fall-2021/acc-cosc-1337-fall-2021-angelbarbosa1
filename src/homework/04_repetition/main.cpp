6fa357854e79e3a4a8defbbfec3dce070f69045c
angelbarbosa1 <angel.barbosaolivares@g.austincc.edu>
2021-10-01 17:57:52 -0500

    hw4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,6 +1,13 @@
 //write include statements
+#include<iostream>
+#include<string>
+#include"dna.h"
 
 //write using statements
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
 
 /*
@@ -12,5 +19,49 @@ user enters a y or Y.
 */
 int main() 
 {
+	char confirmation = 'n';
+	int user_choice = 0;
+	int user_input_num1;
+	int user_input_num2;
+	int results;
+
+	do {
+		cout << "_____MAIN MENU_____"<< endl;
+		cout << "1-Factorial" << endl;
+		cout << "2-Greatest Common Divisor" << endl;
+		cout << "3-Exit" << endl;
+		cout<< "___________________________" << endl;
+		cout << "Make a selection: ";
+		cin >> user_choice;
+		
+
+		if (user_choice == 1) {
+			cout << "What number do you want to calculate factorial for? ";
+			cin >> user_input_num1;
+			results = factorial(user_input_num1);
+			cout << "Factorial for " << user_input_num1 << " is " << results << "." << endl;
+			cout << " " << endl; 
+		}
+
+		if (user_choice == 2) {
+			cout << "Enter first number: ";
+			cin >> user_input_num1;
+			cout << "Enter second number: ";
+			cin >> user_input_num2;
+			results = gcd(user_input_num1, user_input_num2);
+			cout << "The GCD for " << user_input_num1 << " and " << user_input_num2 << " is " << results << "." << endl;
+			cout << " " <<endl; 
+		}
+
+		if (user_choice == 3) {
+			cout << "You are sure you want to exit?   ";
+			cin >> confirmation;
+			if ((confirmation == 'y') || (confirmation == 'Y')) {
+				cout << "End" << endl;
+				return 0;
+			}
+		}
+		
+	} while ((confirmation != 'Y') && (confirmation != 'y'));
 	return 0;
 }
