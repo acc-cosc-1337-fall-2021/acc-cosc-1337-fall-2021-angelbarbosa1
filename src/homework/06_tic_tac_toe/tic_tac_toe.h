9f21b65af1b4226fcd51105790e492f50d6cb1ba
angelbarbosa1 <angel.barbosaolivares@g.austincc.edu>
2021-10-29 22:05:57 -0500

    hw-7

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,3 +1,4 @@
+// include
 #include<string>
 #include<vector>
 
@@ -6,19 +7,26 @@ using std::string; using std::vector;
 class TicTacToe
 {
     public:
-        bool game_over();
-        void start_game(string first_player);
-        void mark_board(int position);
-        void display_board()const;
-        string get_player()const;
-        
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    void display_board()const;
+    string get_player()const;
+    string get_winner();
+    
     private:
-        string player;
-        vector<string> pegs = vector<string>(9," ");
+        
+    string player;
+    string winner;
+    vector<string> pegs = vector<string>(9," ");
 
-        bool check_board_full();
-        void set_next_player();
-        void clear_board();
+    bool check_board_full();
+    bool check_column_win();
+    bool check_row_win();
+    bool check_diagonal_win();
+    void set_next_player();
+    void set_winner();
+    void clear_board();    
 };
 
 void strToUpper(string &str);
